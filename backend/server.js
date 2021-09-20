c0fb51cda0b233cb4e7750ba4f2234c848545863
Dan Martin <danearlcmartin@gmail.com>
2021-09-20 15:47:46 +0300

    user Model

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,41 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use((req,res,next) => {
-    console.log('hello')
-    next()
-})
+app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.send('API is running!')
-})
+app.use((req, res, next) => {
+  console.log('hello');
+  next();
+});
 
-app.use('/api/products',productRoutes)
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
 
-app.use(notFound)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(errorHandler)
+app.use(notFound);
 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
-
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Header></Header>
       <main className='py-3'>
         <Container>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/Product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
